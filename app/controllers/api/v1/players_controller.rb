77a4629fa730b49defecd530152807fc47791e2d
Lou Alicegary <lou@ginkgotree.com>
2015-11-20 00:21:16 -0500

    api/v1/players/create endpoint

--- app/controllers/api/v1/players_controller.rb
+++ app/controllers/api/v1/players_controller.rb
@@ -6,4 +6,19 @@ class Api::V1::PlayersController < ApplicationController
   end
 
 
+  def create
+    player = Player.create(prep_create_params)
+    render json: player # This is processed by an Active Model serializer in app/serializers
+  end
+
+
+  private
+
+    def prep_create_params
+      params[:player][:mu] = 25
+      params[:player][:sigma] = 25.0/3.0
+      params.require(:player).permit(:name, :mu, :sigma)
+    end
+
+
 end
