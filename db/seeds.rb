c76af43420bc6751dce536d84e4b7ee7cb9b7f1e
Lou Alicegary <lou@ginkgotree.com>
2015-11-21 10:12:51 -0500

    Removing bang from delete_all

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,10 @@
-Group.delete_all!
+Group.delete_all
 Group.create!([
   {id: 1, name: "Drive"},
   {id: 2, name: "Walk"}
 ])
 
-Player.delete_all!
+Player.delete_all
 Player.create!([
   {id: 1, name: "abe", mu: "25", sigma: "8.33"},
   {id: 2, name: "boris", mu: "25", sigma: "8.33"},
@@ -12,7 +12,7 @@ Player.create!([
   {id: 4, name: "diana", mu: "25", sigma: "8.33"},
 ])
 
-GroupMembership.delete_all!
+GroupMembership.delete_all
 GroupMembership.create!([
   {group_id: 1, player_id: 1},
   {group_id: 1, player_id: 2},
@@ -22,7 +22,7 @@ GroupMembership.create!([
   {group_id: 2, player_id: 4},
 ])
 
-DoublesMatch.delete_all!
+DoublesMatch.delete_all
 DoublesMatch.create!([
   {winner_1: 1, winner_2: 3, loser_1: 2, loser_2: 4},
   {winner_1: 1, winner_2: 3, loser_1: 2, loser_2: 4},
@@ -32,7 +32,7 @@ DoublesMatch.create!([
   {winner_1: 1, winner_2: 3, loser_1: 2, loser_2: 4},
 ])
 
-SinglesMatch.delete_all!
+SinglesMatch.delete_all
 SinglesMatch.create!([
   {winner: 1, loser: 2},
   {winner: 1, loser: 4},
